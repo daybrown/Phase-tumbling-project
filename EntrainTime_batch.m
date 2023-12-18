f5d5ef19ff764a72e1cd3e8cac6db70112c72716
daybrown <42586088+daybrown@users.noreply.github.com>
2023-12-18 23:03:24 +0800

    Update EntrainTime_batch.m

--- EntrainTime_batch.m
+++ EntrainTime_batch.m
@@ -75,6 +75,7 @@ Etime_cd.mesh = {xx,yy};
 % Etime_cd.coupling = 'bidirectional';\
 
 %% Plot three types of entrain time 
+load('Etime_cd_20230805.mat');
 figure;
 fig = tiledlayout(4,3);
 xx = Etime_cd.mesh{1};
